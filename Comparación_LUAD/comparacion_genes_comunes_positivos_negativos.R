043392680bb6ff7498f25b7e95b551c792b891f5
Nayade Sarmiento <nayade.sarmiento@gmail.com>
2025-01-28 16:53:36 +0100

    Add files via upload

---
+++ Comparación_LUAD/comparacion_genes_comunes_positivos_negativos.R
@@ -0,0 +1,30 @@
+rm(list=ls())
+setwd("~/MASTER/TESIS/comparacion_LUAD")
+
+# Cargar las bibliotecas necesarias
+library(dplyr)
+
+# Función principal
+comparar_genes <- function(archivo1, archivo2, output_file) {
+  # Leer los archivos CSV
+  data1 <- read.csv(archivo1, stringsAsFactors = FALSE)
+  data2 <- read.csv(archivo2, stringsAsFactors = FALSE)
+  
+  # Filtrar los genes comunes
+  genes_comunes <- intersect(data1$Genes_Comunes, data2$Genes_Comunes)
+  
+  # Crear un nuevo data frame con los genes comunes
+  resultado <- data.frame(Genes_Comunes = genes_comunes)
+  
+  # Guardar el resultado en un archivo CSV
+  write.csv(resultado, output_file, row.names = FALSE)
+  
+  cat("Archivo generado exitosamente en:", output_file, "\n")
+}
+
+# Reemplazar con las rutas reales de los archivos
+archivo1 <- "genes_comunes_positivos_LUAD.csv"
+archivo2 <- "genes_comunes_negativos_LUAD.csv"
+output_file <- "genes_comunes_positivos_negativos_LUAD.csv"
+
+comparar_genes(archivo1, archivo2, output_file)
